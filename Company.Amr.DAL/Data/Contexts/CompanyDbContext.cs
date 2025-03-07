5a0d57c6c7d09443d475ddf668c56fb053622c48
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-03-07 21:49:11 +0200

    Implementation Of DAL

---
+++ Company.Amr.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Amr.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Amr.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public DbSet<Department> departments { get; set; }
+
+        public CompanyDbContext() : base()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = AMRAHMED\\MSSQLSERVER01; Database = CompanyDb; Trusted_Connection = True ; TrustServerCertificate=True");
+        }
+    }
+}
