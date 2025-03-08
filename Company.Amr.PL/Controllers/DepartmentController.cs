31d7b92a50428d9a7aa481b87075dfd937170be5
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-03-08 22:10:34 +0200

    Create Create(Action,View)

--- Company.Amr.PL/Controllers/DepartmentController.cs
+++ Company.Amr.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.Amr.BLL.Interfaces;
 using Company.Amr.BLL.Repositories;
+using Company.Amr.DAL.Models;
+using Company.Amr.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Amr.PL.Controllers
@@ -21,6 +23,31 @@ namespace Company.Amr.PL.Controllers
             var departments = _departmentRepository.GetAll();
 
             return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if(ModelState.IsValid) // Server Side Validation
+            {
+                var department = new Department() 
+                {
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = DateTime.Now
+                };
+                
+                var count = _departmentRepository.Add(department);
+                if(count > 0) return RedirectToAction(nameof(Index));
+            }
+
+
+            return View();
         }
     }
 }
