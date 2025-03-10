47a3a719b3ae5e23829c6afae1bb4347354524a8
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-03-10 20:42:47 +0200

    Implmentation Of DAL For Employee Service

---
+++ Company.Amr.DAL/Data/Configurations/EmployeeConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Amr.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Company.Amr.DAL.Data.Configurations
+{
+    public class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)"); 
+        }
+    }
+}
