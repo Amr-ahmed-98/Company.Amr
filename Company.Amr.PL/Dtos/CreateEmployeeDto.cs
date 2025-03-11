1bc6fa1967ee86632c575bd8d12f6e7fd7c48938
Amr-ahmed-98 <67926050+Amr-ahmed-98@users.noreply.github.com>
2025-03-11 21:17:11 +0200

    Implementation of PL For Employee Service

---
+++ Company.Amr.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.Amr.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Range(18, 60, ErrorMessage = "Age must be between 18 and 60")]
+        public int? Age { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$", ErrorMessage = "Address Must be in this format: 123-StreetName-City-Country")]
+        public string Address { get; set; }
+
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone Number")]
+        public string Phone { get; set; }
+
+        [DataType(DataType.Currency, ErrorMessage = "Invalid Salary")]
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+
+        [DisplayName("Hiring Date")]
+        public DateTime HiringDate { get; set; }
+        [DisplayName("Date of Creation")]
+        public DateTime CreateAt { get; set; }
+    }
+}
